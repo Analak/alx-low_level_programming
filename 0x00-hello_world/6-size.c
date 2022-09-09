73051694251d454adb9e6ec66dd301831cc7abec
Analak <nerolinmike@gmail>
2022-09-08 20:48:22 -0700

    C is fun again at last

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	printf("          int is %2d byte(s) \n", sizeof(int));
-	printf("     long int is %2d byte(s) \n", sizeof(long int));
-	printf("  long long int is %2d byte(s) \n", sizeof(long long int));
-	printf("        float is %2d byte(s) \n", sizeof(float));
-	printf("         char is %2d byte(s) \n", sizeof(char));
+	printf("  Size of a char: %2d byte(s) \n", sizeof(char));
+	printf("  Size of an int: %2d byte(s) \n", sizeof(int));
+	printf("  Size of a long int: %2d byte(s) \n", sizeof(long int));
+	printf("  Size of a long long int:%2d byte(s) \n", sizeof(long long int));
+	printf("  Size of a float:%2d byte(s) \n", sizeof(float));
 	return (0);
 }
