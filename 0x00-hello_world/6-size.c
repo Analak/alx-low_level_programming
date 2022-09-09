bc319f8719388e0b79e93f634f358430dd6ca9fe
Analak <nerolinmike@gmail>
2022-09-08 19:24:30 -0700

    C is fun again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("    short int is %2d byte(s) \n", sizeof(short int));
 	printf("          int is %2d byte(s) \n", sizeof(int));
 	printf("     long int is %2d byte(s) \n", sizeof(long int));
-	printf("   signed int is %2d byte(s) \n", sizeof(signed int));
-	printf(" unsigned int is %2d byte(s) \n", sizeof(unsigned int));
-	printf("\n");
+	printf("  long long int is %2d byte(s) \n", sizeof(long long int));
 	printf("        float is %2d byte(s) \n", sizeof(float));
-	printf("       double is %2d byte(s) \n", sizeof(double));
-	printf("  long double is %2d byte(s) \n", sizeof(long double));
-	printf("\n");
-	printf("  signed char is %2d byte(s) \n", sizeof(signed char));
 	printf("         char is %2d byte(s) \n", sizeof(char));
-	printf("unsigned char is %2d byte(s) \n", sizeof(unsigned char));
 	return (0);
 }
